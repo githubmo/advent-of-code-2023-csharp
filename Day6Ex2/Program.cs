efa46594825bbdbc5f93873c83fba98e5fdf8c0b
Mohammed Abdulrazeg <github@mzawi.me>
2023-12-06 12:14:30 +0000

    Day6Ex2 done

---
+++ Day6Ex2/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections.Concurrent;
+using System.Text;
+using System.Text.RegularExpressions;
+
+// it's only two lines
+var lines = File.ReadLines("Resources/input.txt", Encoding.UTF8).ToList();
+var numRegex = new Regex(@"[^\d]+");
+Console.WriteLine(numRegex);
+var time = long.Parse(string.Join("", numRegex.Split(lines[0]).Where(d => !string.IsNullOrWhiteSpace(d))));
+var distance = long.Parse(string.Join("", numRegex.Split(lines[1]).Where(d => !string.IsNullOrWhiteSpace(d))));
+var cb = new ConcurrentBag<long>();
+
+var (min, max) = (0L, 0L);
+for (var l = 1L; l < time; l++)
+{
+    var t = time - l;
+    if (t * l > distance)
+    {
+        min = l;
+        break;
+    }
+}
+
+for (var l = time; l > 1; l--)
+{
+    var t = time - l;
+    if (t * l > distance)
+    {
+        max = l;
+        break;
+    }
+}
+
+Console.WriteLine(max - min + 1); // +1 to include the 0th element
