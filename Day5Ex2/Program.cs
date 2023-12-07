577c3df3f75c486c6c7cd9dee4bd48c30c999c07
Mohammed Abdulrazeg <github@mzawi.me>
2023-12-07 23:21:56 +0000

    adding more parallelism

--- Day5Ex2/Program.cs
+++ Day5Ex2/Program.cs
@@ -1,9 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Collections.Concurrent;
+using System.Diagnostics;
 using System.Text;
 using System.Text.RegularExpressions;
 
+var stopWatch = new Stopwatch();
+stopWatch.Start();
 const string inputResource = "Resources/input.txt";
 var lines = File.ReadLines(inputResource, Encoding.UTF8);
 
@@ -53,30 +56,43 @@ foreach (var line in lines)
 var cb = new ConcurrentBag<long>();
 Parallel.ForEach(sourceSteps, range =>
 {
-    var localMin = long.MaxValue;
-    for (var l = range.From; l <= range.To; l++)
+    Parallel.ForEach(FromTo(range.From, range.To)
+        .GroupBy(x => x / 100000000)
+        .Select(x => x.Select(y => y)), longs =>
     {
-        var currentL = l;
-        foreach (var ms in mappingsToRanges)
+        var localMin = long.MaxValue;
+
+        foreach (var l in longs)
         {
-            var captured = ms.Select(m => m.Apply(currentL)).Where(ll => ll != null);
-            var firstOrDefault = captured.FirstOrDefault();
-            currentL = firstOrDefault ?? currentL;
+            var currentL = l;
+            foreach (var ms in mappingsToRanges)
+            {
+                var captured = ms.Select(m => m.Apply(currentL)).Where(ll => ll != null);
+                var firstOrDefault = captured.FirstOrDefault();
+                currentL = firstOrDefault ?? currentL;
+            }
+
+            localMin = Math.Min(currentL, localMin);
         }
 
-        localMin = Math.Min(currentL, localMin);
-    }
-
-    Console.WriteLine(localMin);
-    cb.Add(localMin);
+        cb.Add(localMin);
+    });
 });
-
+//72263011
 Console.WriteLine(cb.Min());
+// Console.WriteLine(sum);
+stopWatch.Stop();
+Console.WriteLine(stopWatch.Elapsed);
 return;
 
 List<long> NumberMappings(string line)
 {
     return line.Trim().Split(" ").Select(long.Parse).ToList();
+}
+
+IEnumerable<long> FromTo(long from, long to)
+{
+    for (var l = from; l < to; l++) yield return l;
 }
 
 internal sealed record SourceRange(long From, long RangeLength)
