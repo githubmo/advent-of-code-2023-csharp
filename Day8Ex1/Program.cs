d8f7e1a819bc19cdb4050b4b578f389cc9a16ab8
Mohammed Abdulrazeg <github@mzawi.me>
2023-12-08 23:02:05 +0000

    wip

---
+++ Day8Ex1/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using System.Text.RegularExpressions;
+
+var lines = File.ReadLines("Resources/input.txt", Encoding.UTF8);
+var enumerator = lines.GetEnumerator();
+enumerator.MoveNext();
+
+var labelRegex = new Regex(@"(\w{3}) = \((\w{3}), (\w{3})");
+var directions = enumerator.Current.ToCharArray();
+enumerator.MoveNext();
+
+var map = new Dictionary<string, LeftRight>();
+while (enumerator.MoveNext())
+{
+    var groups = labelRegex.Match(enumerator.Current).Groups;
+    var (key, left, right) = (groups[1].Value, groups[2].Value, groups[3].Value);
+    map[key] = new LeftRight(left, right);
+}
+
+
+var steps = 0L;
+
+var current = "AAA";
+while (current != "ZZZ" && steps < 10)
+{
+    // LRLRLLRLRLRRLRLRLRRLRLRLLRRLRRLRLRLRLLR
+    var direction = directions[steps % directions.Length];
+    var options = map[current];
+    current = direction == 'R' ? options.Right : options.Left;
+    Console.WriteLine(current);
+    steps++;
+}
+
+Console.WriteLine(steps);
+
+internal record LeftRight(string Left, string Right);
