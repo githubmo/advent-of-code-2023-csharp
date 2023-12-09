22e7c1f644822c64d997651a7048f8d8e41e6b45
Mohammed Abdulrazeg <github@mzawi.me>
2023-12-09 09:56:59 +0000

    Da9Ex2 done

---
+++ Day9Ex2/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+var lines = File.ReadLines("Resources/input.txt", Encoding.UTF8);
+
+var sum = 0L;
+foreach (var line in lines)
+{
+    var numbs = line.Split(" ").Select(long.Parse).ToList();
+    numbs.Reverse();
+    var number = NextInSeq(numbs);
+    sum += number;
+}
+
+Console.WriteLine(sum);
+
+long NextInSeq(List<long> list)
+{
+    var diffs = list.Zip(list.Skip(1)).Select(pair => pair.Second - pair.First).ToList();
+    // Console.WriteLine(string.Join(" ::: ", diffs));
+    return diffs.All(l => l == 0) ? list.Last() : list.Last() + NextInSeq(diffs);
+}
