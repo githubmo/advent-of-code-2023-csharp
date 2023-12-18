601bb92c8ae1d5a7c4b96b8ceb8196089c4e51e5
Mohammed Abdulrazeg <github@mzawi.me>
2023-12-18 22:00:53 +0000

    Day13Ex2 done

---
+++ Day13Ex2/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+var lines = File.ReadLines("Resources/input.txt", Encoding.UTF8);
+
+var maps = new List<List<string>>();
+var currentMap = new List<string>();
+foreach (var line in lines)
+    if (string.IsNullOrEmpty(line))
+    {
+        maps.Add(currentMap);
+        currentMap = [];
+    }
+    else
+    {
+        currentMap.Add(line);
+    }
+
+maps.Add(currentMap);
+
+var middles = maps.Select(FindMiddle);
+Console.WriteLine(string.Join("\n", middles));
+
+var sum = middles.Sum(m => m.Value);
+Console.WriteLine(sum);
+
+Middle FindMiddle(List<string> map)
+{
+    var column = 0;
+    for (var i = 1; i < map.Count; i++)
+    {
+        var count = Math.Min(i, map.Count - i);
+        var first = map.Slice(i - count, count);
+        var second = map.Slice(i, count);
+        second.Reverse();
+        var diffs = first.Zip(second)
+            .Select(lines =>
+                lines.First.Zip(lines.Second).Count(chars => chars.First != chars.Second))
+            .Sum();
+        if (diffs != 1) continue;
+        column = i;
+        break;
+    }
+
+    var row = 0;
+    var inverseMap =
+        Enumerable
+            .Range(0, map[0].Length)
+            .Select(i => string.Join("", map.Select(s => s[i])))
+            .ToList();
+    for (var i = 1; i < inverseMap.Count; i++)
+    {
+        var count = Math.Min(i, inverseMap.Count - i);
+        var first = inverseMap.Slice(i - count, count);
+        var second = inverseMap.Slice(i, count);
+        second.Reverse();
+        var diffs = first.Zip(second)
+            .Select(lines =>
+                lines.First.Zip(lines.Second).Count(chars => chars.First != chars.Second))
+            .Sum();
+        if (diffs != 1) continue;
+        row = i;
+        break;
+    }
+
+    return new Middle(row, column);
+}
+
+internal record Middle(int Column, int Row)
+{
+    public int Value => 100 * Row + Column;
+}
