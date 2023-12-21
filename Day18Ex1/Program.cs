cf3cf1a613f7c174311978fc98386eaec32c72b1
Mohammed Abdulrazeg <github@mzawi.me>
2023-12-21 17:02:21 +0000

    Day18Ex1 done

--- Day18Ex1/Program.cs
+++ Day18Ex1/Program.cs
@@ -1,16 +1,74 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Text;
+using System.Text.RegularExpressions;
 
-var lines = File.ReadLines("Resources/input.txt", Encoding.UTF8);
+var lines = File.ReadLines("Resources/example.txt", Encoding.UTF8);
+
+var instructionRegex = new Regex(@"(\w)\s+(\d+)\s+\(\#(\w+)\)");
+
 
 var sum = 0;
+var instructionsList = new List<Instruction>();
 foreach (var line in lines)
 {
-    var chars = line.ToList();
-    var numbers = chars.Where(char.IsDigit).Select(c => int.Parse(c.ToString())).ToList();
-    var number = numbers.First() * 10 + numbers.Last();
-    sum += number;
+    var groups = instructionRegex.Match(line).Groups;
+    var instruction = new Instruction(FromChar(groups[1].Value[0]), int.Parse(groups[2].Value), groups[3].Value);
+    // Console.WriteLine(instruction);
+    instructionsList.Add(instruction);
 }
 
-Console.WriteLine(sum);
+var points = new List<(int i, int j)>();
+points.Add((1, 1));
+
+foreach (var newPoint in from instruction in instructionsList
+         let lastPoint = points.Last()
+         let steps = instruction.Steps
+         let direction = instruction.Direction
+         select direction switch
+         {
+             Direction.R => (lastPoint.i, lastPoint.j + steps),
+             Direction.L => (lastPoint.i, lastPoint.j - steps),
+             Direction.D => (lastPoint.i + steps, lastPoint.j),
+             Direction.U => (lastPoint.i - steps, lastPoint.j),
+             _ => throw new ArgumentOutOfRangeException(nameof(direction), "unsupported direction")
+         })
+    points.Add(newPoint);
+
+var pointsInside = CalculateArea();
+var edges = instructionsList.Select(i => i.Steps).Sum();
+
+
+Console.WriteLine(edges / 2 + pointsInside - 1 + 2);
+return;
+
+
+int CalculateArea()
+{
+    double area = 0;
+    var j = points.Count() - 1;
+
+    for (var i = 0; i < points.Count(); i++)
+    {
+        area += (points[j].Item1 + points[i].Item1) * (points[j].Item2 - points[i].Item2);
+        j = i;
+    }
+
+    return (int)Math.Abs(area / 2);
+}
+
+Direction FromChar(char c)
+{
+    Enum.TryParse(c.ToString(), false, out Direction d);
+    return d;
+}
+
+internal enum Direction
+{
+    R = 'R',
+    U = 'U',
+    L = 'L',
+    D = 'D'
+}
+
+internal record Instruction(Direction Direction, int Steps, string Colour);
