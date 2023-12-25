c4f48a3e216b6887889b5ac594f2dc75fefe2a3a
Mohammed Abdulrazeg <github@mzawi.me>
2023-12-25 18:30:23 +0000

    Day23Ex2 not really done but brute forces a right answer

--- Day23Ex2/Program.cs
+++ Day23Ex2/Program.cs
@@ -2,6 +2,10 @@
 
 using System.Text;
 
+// this solution is terrible, takes ages, never finishes but if I break point after leaving it 
+// running for a long time and look at max, i get the right answer
+// i should do better
+
 var lines = File.ReadLines("Resources/input.txt", Encoding.UTF8);
 var map = lines.Select(l => l.ToCharArray()).ToArray();
 var (rows, columns) = (map.Length, map[0].Length);
@@ -18,27 +22,18 @@ for (var j = 0; j < columns; j++)
     {
         var point = new Point(i, j);
         var neighbours = new List<Point>();
-        if (c is '.' or '>')
+        if (c != '#')
         {
             var np = point with { J = point.J + 1 };
             if (IsValid(np)) neighbours.Add(np);
-        }
 
-        if (c is '.' or 'v')
-        {
-            var np = point with { I = point.I + 1 };
+            np = point with { I = point.I + 1 };
             if (IsValid(np)) neighbours.Add(np);
-        }
 
-        if (c is '.' or '<')
-        {
-            var np = point with { J = point.J - 1 };
+            np = point with { J = point.J - 1 };
             if (IsValid(np)) neighbours.Add(np);
-        }
 
-        if (c is '.' or '^')
-        {
-            var np = point with { I = point.I - 1 };
+            np = point with { I = point.I - 1 };
             if (IsValid(np)) neighbours.Add(np);
         }
 
@@ -50,23 +45,44 @@ for (var j = 0; j < columns; j++)
 var paths = new List<List<Point>>();
 paths.Add(new List<Point> { new(0, 1) });
 var lastPoint = new Point(rows - 1, columns - 2); // last point always on last row and second to last column
+var max = 0;
 while (!paths.TrueForAll(l => l.Contains(lastPoint)))
     paths = paths.SelectMany(path =>
     {
-        // if path contains last point, just return it without a new path
-        if (path.Contains(lastPoint)) return [path];
+        // if path contains last point, remove it to save time
+        if (path.Last() == lastPoint)
+        {
+            max = Math.Max(max, path.Count);
+            return [];
+        }
 
         // create a new path for every point that is connecting to the last point in the path
         tiles.TryGetValue(path.Last(), out var tile);
         var validConnected = tile!.Connected.Where(c => !path.Contains(c)).ToList();
-        if (validConnected.Count == 0) return []; // if it cannot connect, it's not a valid path
-        return validConnected.Select(p => path.Concat([p])).ToList();
+        switch (validConnected.Count)
+        {
+            case 0:
+                return []; // if it cannot connect, it's not a valid path
+            case > 1:
+                return validConnected.Select(p => path.Concat([p])).ToList();
+        }
+
+        var ps = new List<Point>();
+        while (validConnected.Count == 1)
+        {
+            ps.AddRange(validConnected);
+            tiles.TryGetValue(ps.Last(), out tile);
+            validConnected = tile!.Connected.Where(c => !path.Contains(c) && !ps.Contains(c)).ToList();
+        }
+
+        return [path.Concat(ps)];
     }).Select(p => p.ToList()).ToList();
 
 // Console.WriteLine(string.Join("\n", tiles.Select(t => $"{t.Key} >> {string.Join(" :: ", t.Value.Connected)}")));
 
 // Finding the one that 
-Console.WriteLine(paths.Max(p => p.Count - 1)); // -1 because the first tile you don't step to it
+max = paths.Count == 0 ? max - 1 : Math.Max(max, paths.Max(p => p.Count)) - 1;
+Console.WriteLine(max); // -1 because the first tile you don't step to it
 
 // a tile is a valid stepping tile if it is within the map and is not a rock '#'
 bool IsValid(Point p)
